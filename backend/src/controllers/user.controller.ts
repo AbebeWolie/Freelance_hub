922e10c8f666a3ef7cd1bceeee92c2d1734dd834
AbebeWolie <106948479+AbebeWolie@users.noreply.github.com>
2025-04-14 21:55:35 +0300

    User controller added

---
+++ backend/src/controllers/user.controller.ts
@@ -0,0 +1,53 @@
+import {Request,Response } from 'express';
+import User from '../models/user.model';
+
+
+// GET ALL USER
+
+const getUser = async (req:Request,res:Response) =>{
+    try{
+        const user = await User.find();
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"User not found"
+            })
+        }
+        return res.status(200).json({
+            success:true,
+            message:"User succefully fetched",
+            data:user
+        })
+    }catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"Internal server error"
+        })
+    };
+}
+
+// GET USER BY ID
+
+const getUserById = async(req:Request,res:Response) =>{
+    try{
+        const user = await User.findById(req.params.id)
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"User not found"
+            })
+        }
+
+        return res.status(200).json({
+            success:true,
+            message:"User successfully fetched",
+            data:user
+        })
+
+    }catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"Internal server error"
+        })
+    }
+}
