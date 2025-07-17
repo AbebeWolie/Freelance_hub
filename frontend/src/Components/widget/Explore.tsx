8aef8b3da57075d8b79bb3328720524d0bc59284
AbebeWolie <abebewolie562@gmail.com>
2025-07-17 05:23:07 +0300

    Explore component updated

--- frontend/src/Components/widget/Explore.tsx
+++ frontend/src/Components/widget/Explore.tsx
@@ -2,8 +2,12 @@ const Explore =()=>{
     return(
         <div className="p-10">
             <div className="flex flex-col justify-end items-center bg-primary-light p-10 rounded-2xl gap-6">
-                <h1 className="text-3xl text-white font-bold font-sans [word-spacing:-2px]">Find your next hire for a short task or long-term growth</h1>
-                <button className="bg-white text-gray-700 p-6 py-1.5 rounded font-semibold text-sm">Explore Freelancers</button>
+                <h1 className="text-3xl text-white font-bold font-sans [word-spacing:-2px]">
+                    Find your next hire for a short task or long-term growth
+                </h1>
+                <button className="bg-white text-gray-700 p-6 py-1.5 rounded font-semibold text-sm">
+                    <a href="#">Explore Freelancers</a>
+                </button>
             </div>
         </div>
     )
